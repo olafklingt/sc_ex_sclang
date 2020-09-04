765d89d647cc80551594685546d83efdae859de0
Olaf Hochherz <olaf@klingt.org>
2020-09-04 19:00:46 +0800

    First commit

---
+++ lib/sc_ex_sclang.ex
@@ -0,0 +1,18 @@
+defmodule ScExSclang do
+  @moduledoc """
+  Documentation for `ScExSclang`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScExSclang.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sc_ex_sclang_test.exs
@@ -0,0 +1,8 @@
+defmodule ScExSclangTest do
+  use ExUnit.Case
+  doctest ScExSclang
+
+  test "greets the world" do
+    assert ScExSclang.hello() == :world
+  end
+end
